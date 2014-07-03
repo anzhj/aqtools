ad40924e38f1b6767d7de3350e1227166ae1c0c2
anzhj <aqhistory@gmail.com>
2014-07-03 20:43:31 +0800

    add log for upload tile tools

--- python/mongo/uptile2qiniu/uptile2qn.py
+++ python/mongo/uptile2qiniu/uptile2qn.py
@@ -5,15 +5,17 @@
 __author__ = 'anzhj'
 
 import sys
+import os
 import cStringIO
 import pymongo
 import base64
+import logging
 from qiniu import rs
 from qiniu import conf
 from qiniu import io
 
 
-# config
+# config information
 conf.ACCESS_KEY = os.getenv("QINIU_ACCESS_KEY")
 conf.SECRET_KEY = os.getenv("QINIU_SECRET_KEY")
 bucket_name = os.getenv("QINIU_BUCKET")
@@ -30,24 +32,50 @@ uptoken = policy.token()
 extra = io.PutExtra()
 extra.mime_type = "application/octet-stream"
 
+
+# log infomation
+logger = logging.getLogger('anzhj@gvitech') 
+logger.setLevel(logging.DEBUG) 
+   
+fh = logging.FileHandler('run.log') 
+fh.setLevel(logging.INFO) 
+
+fherr = logging.FileHandler('run.err')
+fherr.setLevel(logging.ERROR)
+   
+ch = logging.StreamHandler() 
+ch.setLevel(logging.DEBUG) 
+   
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s') 
+fh.setFormatter(formatter) 
+ch.setFormatter(formatter)
+fherr.setFormatter(formatter)
+
+logger.addHandler(fh) 
+logger.addHandler(ch) 
+logger.addHandler(fherr) 
+
+# appliaction logic
 class Up2qn:
 	
 
 	def __init__(self):
 		'''Initializes the upload tools.'''
-		print 'Initializing up2qn.\n'
+		logger.debug( 'Initializing up2qn.\n' )
 		pass
 
 	def __del__(self):
 		'''Unitializes upload tools'''
-		print 'Unitializes.\n'
+		logger.debug( 'Unitializes.\n' )
 
 	def writeQnTile(self, key, mf):
-		ret, err = io.put(uptoken, key, mf, extra)
-		if err is not None:
-			sys.stderr.write('error: %s ' % err)
-			print err
-		print ret
+		while True:
+			ret, err = io.put(uptoken, key, mf, extra)
+			if err is not None:
+				logger.error(err)
+			else:
+				logger.info(ret)
+				break
 
 	def writeQnM(self, strdata):
 		tmp = base64.encodestring(strdata)
